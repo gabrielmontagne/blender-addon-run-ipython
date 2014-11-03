ef07caac5071d5a9ad237d6ce32edb2acd776abe
Gabriel Montagné Láscaris-Comneno <gabriel@rojored.com>
2014-11-03 12:08:27 +0000

    Implment "run IPython" operator.

--- development_run_ipython.py
+++ development_run_ipython.py
@@ -1,14 +1,30 @@
 bl_info = {
-  "name": "Run IPython in terminal",
-  "author": "Gabriel Montagné",
-  "warning": "You should be running Blender with IPython in your python path.",
+  "name": "Run IPython in console",
+  "author": "Gabriel Montagné Láscaris-Comneno",
+  "warning": "You should be running Blender with IPython in your Python path."
+              "Blender won't re-render until you're done!",
   "category": "Development",
 }
 
 import bpy
 
+class RunIPythonOperator(bpy.types.Operator):
+    """Run IPython Operator"""
+    bl_idname = "run_ipython.run"
+    bl_label = "Run IPython in console"
+
+    def execute(self, context):
+        try:
+            import IPython
+            IPython.embed()
+        except:
+            self.report({'ERROR'}, "Couldn't import IPython")
+            return {'CANCELLED'}
+        else:
+            return {'FINISHED'}
+
 def register():
-    print('Register IPython addon')
+    bpy.utils.register_class(RunIPythonOperator)
 
 def unregister():
     print('Unregister run IPython addon')
